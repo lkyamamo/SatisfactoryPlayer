6a833990fb060fed0c39976e8d72fb25bb13e497
Logan Yamamoto Mac <lkyamamo@usc.edu>
2024-11-26 21:04:28 -0800

    simple simon says game starting with playing only 1 round at a time

---
+++ simon-says-ai/simon-says.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import 
+
+WIDTH = 500
+HEIGHT = 500
+DIMENSION = 3
+
+
+class Board:
+    def __init__(self):
+
+        # size of grid DIMENSION X DIMENSION
+        self.dim = DIMENSION
+        self.tiles = 
+
+
+
+tk._test()
